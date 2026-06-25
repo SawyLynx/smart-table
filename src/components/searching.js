4d4819ef88a41bbeff28aaac37045b4f87065b79
Sofia <sofiya040705@gmail.com>
2026-06-25 12:14:38 +0300

    project-work smart-table 4.3

--- src/components/searching.js
+++ src/components/searching.js
@@ -1,9 +1,7 @@
 export function initSearching(searchField) {
-  return (query, state) => {
-    // result заменили на query
+  return (query, state, action) => {
     return state[searchField]
-      ? Object.assign({}, query, {
-          // проверяем, что в поле поиска было что-то введено
+      ? Object.assign({}, query, { // проверяем, что в поле поиска было что-то введено
           search: state[searchField], // устанавливаем в query параметр
         })
       : query; // если поле с поиском пустое, просто возвращаем query без изменений
